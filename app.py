64279065dcb543818a2ffabef1f243bb8b6a0a02
Meraz <meraz.mamun88@myhunter.cuny.edu>
2017-12-05 15:30:20 -0500

    Merge branch 'master' of github.com:gp70/project

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def main():
+    # Initialization
+    # Input
+    if ("R" or "S" or "P" or "Do Something") in request.form:
+        print("Success")
     return render_template('mainpage.html')
 
 app.run(debug=True,host='0.0.0.0',port=5000)
