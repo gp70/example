4f36d7875ae94ce93ca89756f9b3ae0d82ac0086
Gareth <gareth.petterson70@myhunter.cuny.edu>
2017-12-09 16:45:16 -0500

    stats on main page, formatting

--- app.py
+++ app.py
@@ -1,17 +1,78 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import rps
 app = Flask(__name__)
+
+# Variables
+
+pwcount = 0
+cwcount = 0
+tcount = 0
+    
+prcount = 0
+ppcount = 0
+pscount = 0
+crcount = 0
+cpcount = 0
+cscount = 0
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
     # Initialization
+    
+    global pwcount,cwcount,tcount,prcount,ppcount,pscount,crcount,cpcount,cscount
+    
     if request.method == 'GET':
         return render_template('mainpage.html')
+    
     # Input
+    
     if request.method == 'POST':
         choice = request.form['choice']
         weight = request.form['weight']
+        
+        # Easter Egg
+        if ('doot' in choice.lower()) or ('doot' in weight.lower()):
+            return redirect('http://www.reddoot.com', code = 302)
+        
         winner = rps.refine(choice,weight)
-        return render_template('mainpage.html', choice = choice, winner = winner)
+        
+        if winner[0] == 'Player':
+            pwcount += 1
+        if winner[0] == 'Computer':
+            cwcount += 1
+        if winner[0] == 'Nobody':
+            tcount += 1
+            
+        if choice.lower() == 'rock':
+            prcount += 1
+        if choice.lower() == 'paper':
+            ppcount += 1
+        if choice.lower() == 'scissors':
+            pscount += 1
+            
+        if winner[1].lower() == 'rock':
+            crcount += 1
+        if winner[1].lower() == 'paper':
+            cpcount += 1
+        if winner[1].lower() == 'scissors':
+            cscount += 1
+            
+        return render_template('mainpage.html', choice = choice, cchoice = winner[1],
+                                                winner = winner[0], prcount = prcount,
+                                                ppcount = ppcount, pscount = pscount,
+                                                crcount = crcount, cpcount = cpcount,
+                                                cscount = cscount, pwcount = pwcount,
+                                                cwcount = cwcount, tcount = tcount)
+    
+@app.route('/stats')
+def stats():
+    
+    global pwcount,cwcount,tcount,prcount,ppcount,pscount,crcount,cpcount,cscount
+    
+    return render_template('stats.html', prcount = prcount, ppcount = ppcount,
+                                         pscount = pscount, crcount = crcount,
+                                         cpcount = cpcount, cscount = cscount,
+                                         pwcount = pwcount, cwcount = cwcount,
+                                         tcount = tcount)
 
 app.run(debug=True,host='0.0.0.0',port=5000)
--- rps.py
+++ rps.py
@@ -1,50 +1,45 @@
 import random
 
 def refine(p,w):
-    weights = {'normal weights':('R','P','S'),
+    weights = {'normal':('R','P','S'),
                'rock focused':('R','R','P','S'),
                'paper focused':('R','P','P','S'),
-               'scissors focused':('R','P','S','S')}
+               'scissors focused':('R','P','S','S'),
+               'realistic':('R','R','R','R','R','R','R',
+                            'P','P','P','P','P','P','P',
+                            'S','S','S','S','S','S')}
     wlist = []
     for key in weights:
         wlist.append(key)
     if w.lower() not in wlist:
-        w = 'Normal Weights'
+        w = 'Normal'
     if p.lower() == 'rock':
         return decision('R',random.choice(weights[w.lower()]))
     if p.lower() == 'paper':
         return decision('P',random.choice(weights[w.lower()]))
     if p.lower() == 'scissors':
         return decision('S',random.choice(weights[w.lower()]))
-    return 'Type \"Rock,\" \"Paper,\" or \"Scissors,\" please! Nobody'
+    return ('Type \"Rock,\" \"Paper,\" or \"Scissors,\" please! Nobody', 'Nothing')
 
 def decision(p,c):
-    p_count = 0
-    c_count = 0
     if (p == 'R'):
         if (c == 'R'):
-            return 'Nobody'
+            return ('Nobody','Rock')
         if (c == 'P'):
-            c_count += 1
-            return 'Computer'
+            return ('Computer','Paper')
         if (c == 'S'):
-            p_count += 1
-            return 'Player'
+            return ('Player','Scissors')
     if (p == 'P'):
         if (c == 'R'):
-            p_count += 1
-            return 'Player'
+            return ('Player','Rock')
         if (c == 'P'):
-            return 'Nobody'
+            return ('Nobody','Paper')
         if (c == 'S'):
-            c_count += 1
-            return 'Computer'
+            return ('Computer','Scissors')
     if (p == 'S'):
         if (c == 'R'):
-            c_count += 1
-            return 'Computer'
+            return ('Computer','Rock')
         if (c == 'P'):
-            p_count += 1
-            return 'Player'
+            return ('Player','Paper')
         if (c == 'S'):
-            return 'Nobody'
+            return ('Nobody','Scissors')
